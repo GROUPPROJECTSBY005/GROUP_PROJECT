d315172eeb285468f6919b8e41258d0c983d9d67
Ridho Rambe <msyrre29@gmail.com>
2024-06-20 05:57:38 +0700

    Feat : Login & Register, + socket initialization on server

---
+++ SERVER/app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const router = require('./routes')
+
+
+const app = express()
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin : "http://localhost:5173"
+    }
+});
+const port = 3000
+
+
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+io.on("connection", (socket) => {
+    // ...
+  });
+
+app.use(router)
+
+
+httpServer.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
