d47352c7acaf27fb116dce605ef73d2a7d5a4f64
fitra <melania.fitra@gmail.com>
2024-06-20 13:33:11 +0700

    feat :menambahkan fitur daftar user yg online

--- CLIENT/src/pages/Login.jsx
+++ CLIENT/src/pages/Login.jsx
@@ -1,7 +1,69 @@
-export default function Login(){
-    return (
-        <>
-        <h1>Login</h1>
-        </>
-    )
+import { useState } from "react";
+
+export default function Login() {
+
+
+
+    const [username , setUsername] = useState('')
+    const [password , setPassword] = useState('')
+
+
+    const loginHandler = (e)=>{
+e.preventDefault()
+        // console.log(username,'user')
+        localStorage.username = username
+    }
+
+
+
+
+
+  return (
+    <>
+      <h1>Login</h1>
+      <form onSubmit={loginHandler}>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            value={username}
+            onChange={(e)=>{setUsername(e.target.value)}}
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        <div className="mb-3 form-check">
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+            value={password}
+            onChange={(e)=>{setPassword(e.target.value)}}
+          />
+          <label className="form-check-label" htmlFor="exampleCheck1">
+            Check me out
+          </label>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </>
+  );
 }
--- SERVER/app.js
+++ SERVER/app.js
@@ -1,30 +1,41 @@
-const express = require('express')
+const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
-const router = require('./routes')
+const router = require("./routes");
 
-
-const app = express()
+const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-    cors: {
-        origin : "http://localhost:5173"
-    }
+  cors: {
+    origin: "http://localhost:5173",
+  },
 });
-const port = 3000
+const port = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-
+let user = []
 io.on("connection", (socket) => {
-    // ...
-  });
+  // ...
 
-app.use(router)
+  // console.log( socket.handshake.auth.username  , '<<<--username')
+  if (socket.handshake.auth.username)
+    user.push({
+      id: socket.id,
+      username: socket.handshake.auth.username,
+    });
 
+    socket.on("disconnect", () => {
+      user = user.filter(item =>  item.id !== socket.id)
+      io.emit('users:online' , user)
+    });
+    // console.log(user)
+    io.emit('users:online' , user)
+});
+
+app.use(router);
 
 httpServer.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
