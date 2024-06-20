e3bfbc050c84811dd5f581bfbd39c223c5ee52ce
fitra <melania.fitra@gmail.com>
2024-06-20 14:25:01 +0700

    fix : bug di daftar online player

--- CLIENT/src/pages/Register.jsx
+++ CLIENT/src/pages/Register.jsx
@@ -33,7 +33,7 @@ export default function Register() {
           });
           navigate("/login");
         } catch (error) {
-          console.log(error);
+          console.log(error ,'<---');
           Swal.fire({
             icon: 'error',
             title: 'Registration Failed',
